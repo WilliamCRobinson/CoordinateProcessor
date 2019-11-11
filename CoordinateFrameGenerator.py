e267a5ae7a88509bb37cf4d44b341982e90eabf2
prowill <37221762+prowill@users.noreply.github.com>
2019-11-10 18:26:42 -0700

    Add files via upload
    Added command line input arguments, the file to processed is the argument right after the name of the module.

--- CoordinateFrameGenerator.py
+++ CoordinateFrameGenerator.py
@@ -3,7 +3,8 @@ Author: William Robinson November 2019
 The purpose of this module is to define functions and variables that can pull either the x, the y or the z, coordinates
 in sets of frames from MDCRD files.
 """
-
+#import the system module to take command line arguments
+import sys
 # I like to think I've been clear but if anything is confusing don't hesitate to reach out to me. :)
 
 
@@ -36,12 +37,13 @@ def file_appender(file_name, line_array, index_of_line_array):
     except:
         print("something went wrong with file_appender.")
 
+
 # This function is the beef of this module. See Attached readme.txt and comments for details on how this works.
 
 
 def array_processor():
-    # For now just assume the input is always modified_out.crd we can easily change this later on to be cmdline input
-    user_input_file = "modified_out.crd"
+    # Take user input as sys.argv[1] argv[0] is the module name always.
+    user_input_file = sys.argv[1]
     # open file from input in read mode
     coord_to_proc = open(user_input_file, "r")
     # read lines into an array
