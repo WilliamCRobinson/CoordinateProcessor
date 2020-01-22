e093003bc54414f5b9a72e29265b9aacc68586fd
Prowill <william.robinson223@gmail.com>
2020-01-21 21:55:47 -0700

    Fixed yet another minor issue.
    Signed-off-by: Prowill <william.robinson223@gmail.com>

--- run_coord_proc_jobs.py
+++ run_coord_proc_jobs.py
@@ -17,7 +17,7 @@ ext = "run"
 all_runs = [i for i in glob.glob("*.{}".format(ext))]
 # Loop through the run directories
 for run in all_runs:
-    time = str(datetime.datetime.now())
+    time = str(datetime.datetime.now()).split(".")[-1]
     os.chdir(run)
     make_directory(hist_dir)
     shu.copy("mdcrd", hist_dir)
@@ -34,7 +34,7 @@ for run in all_runs:
         file.writelines("#SBATCH --mail-user=wcr9@nau.edu\n")
         file.writelines("module load python/3.latest\n")
         file.writelines("cpptraj -i /home/wcr9/scripts/cpptraj.in -p ../common/cho_ger.parm7\n")
-        file.writelines("python /home/wcr9/scripts/coordinate_processor.py")
+        file.writelines("python /home/wcr9/scripts/coordinate_processor.py\n")
     os.system("sbatch " + run + "coordprocrun.sh")
     os.chdir("../../")
 
