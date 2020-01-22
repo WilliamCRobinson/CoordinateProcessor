d36ecc050ccf75694eb1482335a030d5d33f3828
Prowill <william.robinson223@gmail.com>
2020-01-22 09:52:46 -0700

    Adjusted the way that directories are accessed. I also switched the runtime environment for the python script to my personal seaborn environment on monsoon as monsoon isnt equipped with seaborn.
    Signed-off-by: Prowill <william.robinson223@gmail.com>

--- run_coord_proc_jobs.py
+++ run_coord_proc_jobs.py
@@ -35,7 +35,7 @@ for run in all_runs:
         file.writelines("module load python/3.latest\n")
         file.writelines("module load amber\n")
         file.writelines("cpptraj -i /home/wcr9/scripts/cpptraj.in -p ../../common/cho_ger.parm7\n")
-        file.writelines("python /home/wcr9/scripts/coordinate_processor.py\n")
+        file.writelines(".conda/envs/seaborn_env/python /home/wcr9/scripts/coordinate_processor.py\n")
     os.system("sbatch " + run + "coordprocrun.sh")
     os.chdir("../../")
 
