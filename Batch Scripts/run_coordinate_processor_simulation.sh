2c61e774bee250687c387caea8e54a2662130f07
Prowill <william.robinson223@gmail.com>
2020-02-04 12:01:55 -0700

    Added a bash script that calls our other scripts, this makes it easier to use. Running this should run all those other things. Ideally...
    Signed-off-by: Prowill <william.robinson223@gmail.com>

---
+++ Batch Scripts/run_coordinate_processor_simulation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=T2H
+#SBATCH --time=08:00:00
+#SBATCH --mem=120000
+#SBATCH --output=outT2H
+#SBATCH --error=errorT2H
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=wcr9@nau.edu
+module load amber
+source activate seaborn_env
+python /home/wcr9/scripts/create_cpptraj_combiner.py
+cpptraj -i cpptraj_run_combiner.in -p /common/cho_ger.parm7
+cd ./equil1/
+python /home/wcr9/scripts/create_cpptraj_combiner.py
+cpptraj -i cpptraj_run_combiner.in -p ../common/cho_ger.parm7
+cd ../equil2/
+python /home/wcr9/scripts/create_cpptraj_combiner.py
+cpptraj -i cpptraj_run_combiner.in -p ../common/cho_ger.parm7
+cd ../
+cpptraj -i /home/wcr9/scripts/cpptraj_combine_combined.in -p /common/cho_ger.parm7
+cpptraj -i /home/wcr9/scripts/cpptraj_stripper.in -p /common/cho_ger.parm7
+python /home/wcr9/scripts/coordinate_processor.py
--- create_cpptraj_combiner.py
+++ create_cpptraj_combiner.py
@@ -8,7 +8,7 @@ ext = "run"
 all_runs = [i for i in glob.glob("*.{}".format(ext))]
 all_runs = sorted(all_runs, key=lambda a: int(a.split(".")[0]))
 with open("cpptraj_run_combiner.in","w") as f:
-    f.writelines("parm /common/cho_ger.parm7\n")
+
     for run in all_runs:
         f.writelines("trajin " + run + "/mdcrd\n")
     f.writelines("trajout combined_runs_modified_out\n")
