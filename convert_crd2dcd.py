4c0620e6f74d28281ed4983b52908bf7b4778f6a
Prowill <william.robinson223@gmail.com>
2020-04-05 21:15:03 -0700

    I added a file that converts all the .crd files to .dcd files. It does keep the original crd files though.Converts for both stripped and full trajectory files.
    Signed-off-by: Prowill <william.robinson223@gmail.com>

---
+++ convert_crd2dcd.py
@@ -0,0 +1,14 @@
+"""
+April 2020
+The purpose of this script is to run through the five different simulations in each of the cell membrane types
+and convert the combined e1_e2_main_mdcrd.crd files to .dcd files.
+"""
+import os
+il_conc = [0, 8, 16, 32, 64]
+for element in il_conc:
+    os.chdir(str(element)+"-ILs")
+    os.system("nohup srun --time 05:00:00 --job-name=converter"
+              " cpptraj -i /home/wcr9/scripts/cpptrajcrd2dcd.in -p ./common/cho_ger.parm7")
+    os.system("nohup srun --time 05:00:00 --job-name=stripped_converter"
+              " cpptraj -i /home/wcr9/scripts/cpptrajcrd2dcdstripped.in -p ./strip.cho_ger.parm7")
+    os.chdir("../")
