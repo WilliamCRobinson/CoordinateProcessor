295a4b40f4e5bff3d14c047855f8e198ca6d7837
Prowill <william.robinson223@gmail.com>
2020-04-06 14:14:55 -0700

    small change to the script so that it can submit all jobs at once.
    Signed-off-by: Prowill <william.robinson223@gmail.com>

--- convert_crd2dcd.py
+++ convert_crd2dcd.py
@@ -8,7 +8,7 @@ il_conc = [0, 8, 16, 32, 64]
 for element in il_conc:
     os.chdir(str(element)+"-ILs")
     os.system("nohup srun --time 05:00:00 --job-name=converter"
-              " cpptraj -i /home/wcr9/scripts/cpptrajcrd2dcd.in -p ./common/cho_ger.parm7")
+              " cpptraj -i /home/wcr9/scripts/cpptrajcrd2dcd.in -p ./common/cho_ger.parm7 &")
     os.system("nohup srun --time 05:00:00 --job-name=stripped_converter"
-              " cpptraj -i /home/wcr9/scripts/cpptrajcrd2dcdstripped.in -p ./strip.cho_ger.parm7")
+              " cpptraj -i /home/wcr9/scripts/cpptrajcrd2dcdstripped.in -p ./strip.cho_ger.parm7 &")
     os.chdir("../")
